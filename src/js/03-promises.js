a7213de3cd3be88b3e86b32d5806bbc070a4b37c
Michael <michael.kononenko101@gmail.com>
2022-10-08 15:29:46 +0300

    did refactoring task #3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,9 +1,6 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const delayInput = document.querySelector('input[name=delay]');
-const stepInput = document.querySelector('input[name=step]');
-const amountInput = document.querySelector('input[name=amount]');
-const submitButton = document.querySelector('button[type=submit]');
+const form = document.querySelector('form');
 
 const inputs = {
   delay: 0,
@@ -12,19 +9,14 @@ const inputs = {
   positon: 0,
 };
 
-delayInput.addEventListener(
+form.addEventListener(
   'input',
-  () => (inputs.delay = Number(delayInput.value))
+  event => (inputs[event.target.name] = +event.target.value)
 );
-stepInput.addEventListener(
-  'input',
-  () => (inputs.step = Number(stepInput.value))
-);
-amountInput.addEventListener(
-  'input',
-  () => (inputs.amount = Number(amountInput.value))
-);
-submitButton.addEventListener('click', event => {
+
+form.addEventListener('submit', startTimer);
+
+function startTimer(event) {
   event.preventDefault();
   let newDelay = inputs.delay;
 
@@ -35,21 +27,24 @@ submitButton.addEventListener('click', event => {
     inputs.positon += 1;
     newDelay += inputs.step;
   }
-});
+}
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
+
   const promise = new Promise((resolve, reject) => {
     const result = {
       position,
       delay,
     };
+
     if (shouldResolve) {
       resolve(result);
     } else {
       reject(result);
     }
   });
+
   promise
     .then(({ position, delay }) => {
       setTimeout(
